bcaeb6247f961dc87637de31ed2d801b2a85c45c
PiotrSzwedo <piotrszwedo2007@gmail.com>
2025-06-07 17:39:36 +0200

    Added: db service

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "host" => "db",
+    "port" => "3306",
+    "database" => "testdb",
+    "username" => "root",
+    "password" => "",
+    "charset" => "utf8mb4",
+    "collation" => "utf8mb4_unicode_ci",
+];
