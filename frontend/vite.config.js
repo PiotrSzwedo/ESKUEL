8635bb2c8e3f21e9d94c1336a66a58dfe96f52b9
PiotrSzwedo <piotrszwedo2007@gmail.com>
2025-06-12 22:53:10 +0200

    Added: prototype of frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,24 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  root: 'frontend',
+  base: './',
+  build: {
+    outDir: '../../resources',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        form: path.resolve(__dirname, 'form.ts'),
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './frontend')
+    }
+  }
+})
