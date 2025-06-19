1c59dba90402dc7e8d5d334d8c87dcaaaeb46129
PiotrSzwedo <piotrszwedo2007@gmail.com>
2025-06-19 21:44:23 +0200

    Updated: index

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/routes/web.php';
 
 $configDbPath = __DIR__ . '/config/db.php';
-$path = $_SERVER['REQUEST_URI'];
+if ($routerConfig["path_with_redirection_to_index"] || true){
+    $path = $_SERVER['REQUEST_URI'];
+}else{
+    $path = $_GET['path'];
+}
 
 echo $router->dispatch($path, $_SERVER['REQUEST_METHOD']);
