23dd7a8f41363186e95b95116a434e7b220705bf
PiotrSzwedo <piotrszwedo2007@gmail.com>
2025-06-19 21:44:59 +0200

    Added: router config

--- config/router.php
+++ config/router.php
@@ -1,1 +1,21 @@
 <?php
+
+return [
+    // by default the prefix is
+    // the directory where escuela my admin is installed,
+    // the prefix will not be there if eskuelmyadmin is installed in the root directory of the web server
+    "prefix" => getPrefix(),
+
+    // defines how links are created
+    // by default by redirecting to index
+    "path_with_redirection_to_index" => true
+];
+
+function getPrefix(): string
+{
+    $path = realpath(__DIR__);
+    $parentPath = dirname($path);
+    $relative = str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', $parentPath);
+
+    return $relative;
+}
